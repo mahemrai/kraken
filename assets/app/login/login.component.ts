9c2707e3bbc4835861ee8f424862873a140afdb5
Mahendra Rai <mahemrai@googlemail.com>
2016-08-24 19:10:43 +0100

    initial commit

---
+++ assets/app/login/login.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/login/login.component.html'
+})
+
+export class LoginComponent { }
---
+++ assets/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+const login_component_1 = require('./login/login.component');
+platform_browser_dynamic_1.bootstrap(login_component_1.LoginComponent);
+//# sourceMappingURL=main.js.map
