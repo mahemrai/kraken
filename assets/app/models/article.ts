c0630606c3f56f3c27a9c6dc78d11f95c26604ef
mahemrai <mahemrai@googlemail.com>
2016-08-30 16:58:43 +0100

    updated article model

--- assets/app/models/article.ts
+++ assets/app/models/article.ts
@@ -1,4 +1,15 @@
+import {User} from '../models/user';
+
 export class Article {
     public id:number;
+
+    public title:string;
+
+    public description:string;
+
+    public image:string;
+
     public url:string;
+
+    public sharer:User;
 }
