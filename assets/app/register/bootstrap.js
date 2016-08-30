41c31033780e5193257fac3684bcdc856b25ee04
mahemrai <mahemrai@googlemail.com>
2016-08-30 12:20:49 +0100

    removed all the js and map files for angular app

--- assets/app/register/bootstrap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const register_component_1 = require('../register/register.component');
-const forms_1 = require('@angular/forms');
-const http_1 = require('@angular/http');
-platform_browser_dynamic_1.bootstrap(register_component_1.RegisterComponent, [http_1.HTTP_PROVIDERS, forms_1.disableDeprecatedForms(), forms_1.provideForms()]);
-//# sourceMappingURL=bootstrap.js.map
